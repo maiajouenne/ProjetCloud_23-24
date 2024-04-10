464532b5bf4a9fe82b18ca48a652e01d391d2a1f
maiajouenne <125506491+maiajouenne@users.noreply.github.com>
2024-04-10 12:10:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ def get_anomalies():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=5000)
